254e0f0a86830e4e62029186ceae94960ee71065
Thomas <thomas.dodgson26@gmail.com>
2025-03-19 14:32:13 +1100

    working code

--- tom/src/localization.cpp
+++ tom/src/localization.cpp
@@ -1,16 +1,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include <chrono>
+#include <thread>  // For sleep_for()
 
 class AutoLocalize : public rclcpp::Node {
 public:
     AutoLocalize() : Node("auto_localize") {
         publisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 10);
-        timer_ = this->create_wall_timer(std::chrono::seconds(2), std::bind(&AutoLocalize::set_initial_pose, this));
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // Ensure publisher is fully initialized
+        set_initial_pose();
         RCLCPP_INFO(this->get_logger(), "Localization Node Started");
     }
 
 private:
-//this sets the current coords to be 0,0 if robot is elsewhere this will not work.
     void set_initial_pose() {
         auto msg = geometry_msgs::msg::PoseWithCovarianceStamped();
         msg.header.stamp = this->get_clock()->now();
@@ -20,18 +22,21 @@ private:
         msg.pose.pose.orientation.w = 1.0;
         publisher_->publish(msg);
         RCLCPP_INFO(this->get_logger(), "Published initial pose");
+
+        // Give time for message to be sent before node shuts down
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
 };
-
-
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<AutoLocalize>();
-    rclcpp::spin(node);
+
+    rclcpp::spin_some(node);  // Allow processing of published message
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // Ensure publish is completed
+
     rclcpp::shutdown();
     return 0;
 }
