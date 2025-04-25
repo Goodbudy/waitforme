ad283b7b3c198952e5584cffac7e954835b605c2
Hallie <hallie.r.robins@student.uts.edu.au>
2025-04-25 15:05:08 +1000

    cleaning

--- issy/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/ros2_ws/src/waitforme/issy
-# Build directory: /home/andrew/ros2_ws/src/waitforme/issy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
