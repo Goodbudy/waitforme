0d8eaae5d29306bcd303b3c58d5d4b06a1e488b9
Hrob02 <160565963+Hrob02@users.noreply.github.com>
2025-05-14 15:09:37 +1000

    Update header GoalManager.h for service

--- turtlebot3_multi_robot/src/GoalManager.h
+++ turtlebot3_multi_robot/src/GoalManager.h
@@ -5,6 +5,7 @@
 #include <queue>
 #include <string>
 #include <memory>
+#include <map>
 
 #include "issy/srv/add_goal.hpp"
 #include "issy/srv/execute_goals.hpp"
@@ -13,14 +14,18 @@ class GoalManager : public rclcpp::Node {
 public:
     GoalManager();
 
-    void queue_global_goal(const geometry_msgs::msg::PoseStamped& goal_pose);
+    void queue_global_goal(const geometry_msgs::msg::PoseStamped& goal_pose);  // Optional: for test code
     bool has_global_goals() const;
     void update();
 
 private:
     std::queue<geometry_msgs::msg::PoseStamped> global_goal_queue_;
 
-    rclcpp::Client<issy::srv::AddGoal>::SharedPtr add_goal_client_;
-    rclcpp::Client<issy::srv::ExecuteGoals>::SharedPtr exec_goals_client_;
+    rclcpp::Service<issy::srv::AddGoal>::SharedPtr add_goal_srv_;
+    std::map<std::string, rclcpp::Client<issy::srv::ExecuteGoals>::SharedPtr> exec_goals_client_;
     rclcpp::TimerBase::SharedPtr check_timer_;
+
+    void handleAddGoal(
+        const std::shared_ptr<issy::srv::AddGoal::Request> req,
+        std::shared_ptr<issy::srv::AddGoal::Response> res);
 };
